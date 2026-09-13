2745e84c711f3443e16edb66fdcaabf37fba9c9f
Natalia Trifonova <trifonova.dev@gmail.com>
2026-09-13 14:09:56 +0700

    train

--- src/features/todolists/model/__tests__/todolists-slice.test.ts
+++ src/features/todolists/model/__tests__/todolists-slice.test.ts
@@ -66,7 +66,7 @@ test("correct todolist should change its title", () => {
 
 test("correct todolist should change its filter", () => {
   const filter = "completed"
-  const endState = todolistsSlice(startState, changeTodolistFilterAC({ id: todolistId2, filter }))
+  const endState = todolistsReducer(startState, changeTodolistFilterAC({ id: todolistId2, filter }))
 
   expect(endState[0].filter).toBe("all")
   expect(endState[1].filter).toBe(filter)
